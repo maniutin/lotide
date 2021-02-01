e1c8e987a636f3e4409804a4998a2c79612f6a33
Nick Maniutin <maniutin@gmail.com>
2021-02-01 22:49:58 +0000

    implementing tail

--- tail.js
+++ tail.js
@@ -10,3 +10,14 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const tail = function(array) {
+  return array.slice(1);
+};
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
