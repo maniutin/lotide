f887a6430309b8bf7c0c30d4e1d6bccdab40d5c8
Nick Maniutin <maniutin@gmail.com>
2021-02-04 15:08:11 +0000

    fixed spacing

--- countOnly.js
+++ countOnly.js
@@ -11,11 +11,11 @@ const countOnly = function(allItems, itemsToCount) {
 
   for (const item of allItems){
     if (itemsToCount[item]){
-    if (results[item]){
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-    }
+      if (results[item]){
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
   }
 }
   return results;
