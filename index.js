41f17fd71e766fcbbe373fc3a990c5432e22937f
Nick Maniutin <maniutin@gmail.com>
2021-02-08 22:00:27 +0000

    adds the rest of the functions to index.js

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
-const map = require('./map');
-const letterPositions = require('./letterPositions');
 const flatten = require('./flatten');
 const findKeyByValue = require('./findKeyByValue');
 const findKey = require('./findKey');
 const eqObjects = require('./eqObjects');
 const eqArrays = require('./eqArrays');
-const countOnly = require('./countOnly')
-const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const countletters = require('./letterPositions');
+const letterPositions = require('./letterPositions');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
 
 module.exports = {
-  head,
-  tail,
-  middle,
-  map,
-  letterPositions,
-  flatten,
-  findKeyByValue,
-  findKey,
-  eqObjects,
-  eqArrays,
-  countOnly,
-  countLetters
+head,
+tail,
+middle,
+flatten,
+findKeyByValue,
+findKey,
+eqObjects,
+eqArrays,
+countOnly,
+countletters,
+countindex,
+assertObjectsEqual,
+assertEqual,
+assertArraysEqual,
+without,
+takeUntil,
+map
 };
