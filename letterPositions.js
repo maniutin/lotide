dd2c08bc377f60e7f9b3556114788fafd9bcdc48
Nick Maniutin <maniutin@gmail.com>
2021-02-08 23:49:22 +0000

    refactors letterPositionsTest.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,16 +1,3 @@
-const eqArrays = function(input, compared) {
-  return input.length === compared.length && 
-  input.every((value, index) => value === compared[index])
-};
-
-const assertArraysEqual = function(actual, expected) {
- if (eqArrays(actual, expected)) {
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
- } else {
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
- }
-};
-
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++){
@@ -23,8 +10,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions("hello"))
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
+module.exports = letterPositions;
