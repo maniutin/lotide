9c1b0dc44cf07e6a60956b888803126599fa5f4a
Nick Maniutin <maniutin@gmail.com>
2021-02-08 19:34:38 +0000

    refactors assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,4 @@
-const eqArrays = function(input, compared) {
-  return input.length === compared.length && 
-  input.every((value, index) => value === compared[index])
-};
-
+const eqArrays = require('../eqArrays')
 const assertArraysEqual = function(actual, expected) {
  if (eqArrays(actual, expected)) {
   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -11,8 +7,4 @@ const assertArraysEqual = function(actual, expected) {
  }
 };
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])// => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3])// => false
+module.exports = assertArraysEqual;
